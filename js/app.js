2a67fc4feeffc0455022086ad209fd1a4bfb3554
sahvsergio <sergioandresherrera@gmail.com>
2025-01-01 22:55:27 -0500

    add alerts to app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//advise the person that time is up
+
+alert('Thanks for visiting the Pomodoro App');
+alert("Time is up!");
+console.log('This is an alert');
+document.write('Hello');
+document.querySelector("span").textContent="This is the countdown";
+document.write('<p>This is where the counter will be placed  </p>');
