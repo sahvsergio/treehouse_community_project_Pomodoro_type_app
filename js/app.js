b76a4575ed77b7bf9feffcd9a8d87f1a51f78657
sahvsergio <sergioandresherrera@gmail.com>
2025-01-02 08:23:21 -0500

    add variables with var

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+// completed task
+
+var tasksCompleted;
+var isTaskComplete;
+
 //advise the person that time is up
 
 alert('Thanks for visiting the Pomodoro App');
 alert("Time is up!");
 console.log('This is an alert');
 document.write('Hello');
-document.querySelector("span").textContent="This is the countdown";
+document.querySelector("span").textContent = "This is the countdown";
 document.write('<p>This is where the counter will be placed  </p>');
+
+
+isTaskComplete = true;
+tasksCompleted +=1;
+console.log(isTaskComplete);
