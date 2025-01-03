ddab74a7402d59004bf3e3672c574e13c9ea9a4a
sahvsergio <sergioandresherrera@gmail.com>
2025-01-02 22:54:10 -0500

    add javascript basics examples

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@
 
 let tasksCompleted;
 let isTaskComplete;
+let welcome="Welcome to my  Pomodoro app";
+let countDownText='I\'m'
+
+console.log(countDownText);
+const Multiline="Hello\
+my\
+friend"
+console.log(Multiline)
 
 //advise the person that time is up
 
-alert('Thanks for visiting the Pomodoro App');
+alert(welcome);
 alert("Time is up!");
 console.log('This is an alert');
 document.write('Hello');
@@ -16,3 +24,18 @@ document.write('<p>This is where the counter will be placed  </p>');
 isTaskComplete = true;
 tasksCompleted +=1;
 console.log(isTaskComplete);
+const passphrase='Open Sesame';
+console.log(passphrase.length);
+prompt('Hello');
+// Template literals
+console.log(`Hello ${isTaskComplete} `);
+document.querySelector('h1').innerHTML="Hello"
+let closingTIme=prompt('Closing time');
+
+if(closingTIme=='Welcome'){
+    console.log('That\'s correct')
+
+};
+
+
+
