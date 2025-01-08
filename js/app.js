8b7d12f4f71d1f5dca8a30c367e68d8dcf30c8e2
sahvsergio <sergioandresherrera@gmail.com>
2025-01-07 23:23:31 -0500

    add js buttons and style

--- js/app.js
+++ js/app.js
@@ -1,41 +1,16 @@
-// completed task
+// create an array for tasks
+let tasks=[];
+let buttonNames=['Start Pomodoro', 'Stop Pomodoro', 'Start Brek'];
 
-let tasksCompleted;
-let isTaskComplete;
-let welcome="Welcome to my  Pomodoro app";
-let countDownText='I\'m'
-
-console.log(countDownText);
-const Multiline="Hello\
-my\
-friend"
-console.log(Multiline)
-
-//advise the person that time is up
-
-alert(welcome);
-alert("Time is up!");
-console.log('This is an alert');
-document.write('Hello');
-document.querySelector("span").textContent = "This is the countdown";
-document.write('<p>This is where the counter will be placed  </p>');
-
-
-isTaskComplete = true;
-tasksCompleted +=1;
-console.log(isTaskComplete);
-const passphrase='Open Sesame';
-console.log(passphrase.length);
-prompt('Hello');
-// Template literals
-console.log(`Hello ${isTaskComplete} `);
-document.querySelector('h1').innerHTML="Hello"
-let closingTIme=prompt('Closing time');
-
-if(closingTIme=='Welcome'){
-    console.log('That\'s correct')
-
-};
+function createButtons(arr){
+    let buttons='';
+    for (let i=0;i < arr.length; i++){
+        buttons+=`<button class="btn">${arr[i]}</button>`;
 
 
 
+    }
+    return buttons;
+}
+
+document.querySelector("#buttons").innerHTML=`${createButtons(buttonNames)}`;
